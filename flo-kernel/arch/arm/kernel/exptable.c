066a8e49396020ca7795c7a7a8bb674f93001e7a
Wei Dai <wdai2248@columbia.edu>
2014-11-14 18:14:05 -0500

    test content of pgd

--- flo-kernel/arch/arm/kernel/exptable.c
+++ flo-kernel/arch/arm/kernel/exptable.c
@@ -2,5 +2,12 @@
 #include<linux/unistd.h>
 SYSCALL_DEFINE3(expose_page_table,pid_t,pid,unsigned long, fake_pgd,
 unsigned long, addr) {
+	struct pid *p;
+	struct task_struct *ts;
+
+	p = find_get_pid(pid_t);
+	ts = get_pid_task(p, PIDTYPE_PID);
+	long pgd = ts->mm->pgd;
+	printk("content of pgd: %ld \n", pgd);
 	return 10;		
 }

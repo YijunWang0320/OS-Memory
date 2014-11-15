4bfab5e14d7cec6ed1667591f262c2d49a751718
W4118 Student <w4118@w4118>
2014-11-14 19:37:06 -0500

    test on pgd printk

--- flo-kernel/arch/arm/kernel/exptable.c
+++ flo-kernel/arch/arm/kernel/exptable.c
@@ -5,9 +5,12 @@ unsigned long, addr) {
 	struct pid *p;
 	struct task_struct *ts;
 
-	p = find_get_pid(pid_t);
+	p = find_get_pid(pid);
 	ts = get_pid_task(p, PIDTYPE_PID);
-	long pgd = ts->mm->pgd;
-	printk("content of pgd: %ld \n", pgd);
+	if(ts->mm!=NULL) {
+		unsigned long pgd = ts->mm->pgd[0];
+		unsigned long pgd2 = ts->mm->pgd[1];
+		printk("content of pgd[0]: %lu, pgd[1]= %lu, task:%s \n", pgd, pgd2,ts->comm);
+	}
 	return 10;		
 }

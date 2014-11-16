b5490c725a9fb6d9160e5cce9ca5c0c5a3c623fa
Wei Dai <wdai2248@columbia.edu>
2014-11-15 21:06:33 -0500

    test two level

--- flo-kernel/arch/arm/kernel/exptable.c
+++ flo-kernel/arch/arm/kernel/exptable.c
@@ -18,29 +18,15 @@ unsigned long, addr) {
 
     		if (pgd_none(*pgd) || pgd_bad(*pgd))
         		continue;
-    		
-    		for(j = 0; j < PTRS_PER_PUD;++j)
-    		{
-    			pud_t *pud = pgd + j * PUD_SIZE;
-    			if(pud_none(*pud) || pud_bad(*pud))
-    				continue;
-    			
-    			for(k = 0; k < PTRS_PER_PMD; ++k)
-    			{
-    				pmd_t *pmd = pud + k * PMD_SIZE;
-    				if(pmd_none(*pmd) || pmd_bad(*pmd))
-    					continue;
-    				
     				for(l = 0; l < PTRS_PER_PTE; l++)
     				{
-    					pte_t *pte = pmd + l * PAGE_SIZE;
+    					pte_t *pte = pgd + l * PAGE_SIZE;
     					printk("[index]\t[virt]\t[phys]\t[young bit]\t[file bit]\t[dirty bit]\t[read-only bit]\t[xn bit]\n");
     					printk("%lu\t%X\t%X\t%lu\t%lu\t%lu\t%lu\t%lu\t\n", pte_index(*pte), 0, *pte, pte_young(*pte),pte_file(*pte),pte_dirty(*pte), pte_write(*pte), pte_exec(*pte));
     				}
-    			}
+    			
 
     		}
-		}
 	}
 	return 10;		
 }

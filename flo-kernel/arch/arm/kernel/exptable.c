4564c71b3be5ed568f577fdcdeb717907b76fb74
W4118 Student <w4118@w4118>
2014-11-15 22:39:55 -0500

    test print four level

--- flo-kernel/arch/arm/kernel/exptable.c
+++ flo-kernel/arch/arm/kernel/exptable.c
@@ -12,10 +12,10 @@ unsigned long, addr) {
 	p = find_get_pid(pid);
 	ts = get_pid_task(p, PIDTYPE_PID);
 	if(ts->mm!=NULL) {
-		pgd_t *tmp_pgd = ts->mm->pgd;
-		struct mm_struct *mm = current->mm;
-		struct vm_area_struct *vma = find_vma(mm,fake_pgd);
-		remap_pfn_range(vma,fake_pgd,tmp_pgd,PTRS_PER_PGD*8,VM_READ);
+		//pgd_t *tmp_pgd = ts->mm->pgd;
+		//struct mm_struct *mm = current->mm;
+		//struct vm_area_struct *vma = find_vma(mm,fake_pgd);
+		//remap_pfn_range(vma,fake_pgd,tmp_pgd,PTRS_PER_PGD*8,VM_READ);
 		for (i = 0; i < PTRS_PER_PGD; ++i) {
     			pgd_t *pgd = ts->mm->pgd + i;
     			if (pgd_none(*pgd) || pgd_bad(*pgd))
@@ -26,12 +26,12 @@ unsigned long, addr) {
         				continue;
         			for (k = 0; k < PTRS_PER_PMD; k++) {
         				pmd_t *pmd = pud + k * PMD_SIZE;
-        				if (pmd_none(pmd) || pmd_bad(pmd))
+        				if (pmd_none(*pmd) || pmd_bad(*pmd))
         					continue;
     					for(l = 0; l < PTRS_PER_PTE; l++)
     					{
-    						pte_t *pte = pgd + l * PAGE_SIZE;
-    						if (pte_none(pte) || pte_bad(pte))
+    						pte_t *pte = pmd + l * PAGE_SIZE;
+    						if (pte_none(pte))
         						continue;
     						//printk("[index]\t[virt]\t[phys]\t[young bit]\t[file bit]\t[dirty bit]\t[read-only bit]\t[xn bit]\n");
     						printk("%lu\t%X\t%X\t%lu\t%lu\t%lu\t%lu\t%lu\t\n", pte_index(*pte), 0, *pte, pte_young(*pte),pte_file(*pte),pte_dirty(*pte), pte_write(*pte), pte_exec(*pte));

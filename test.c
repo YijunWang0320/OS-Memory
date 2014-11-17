87e785d86179f22c4ec06bd2f1e27869eded1a4f
xw2353 <xw2353@columbia.edu>
2014-11-16 21:01:08 -0500

    test

--- test.c
+++ test.c
@@ -13,13 +13,13 @@ int main(int argc, char *argv[])
 	fake_pgd = (unsigned long *)mmap(NULL, 2048 * 16, PROT_READ, MAP_SHARED, fd, 0);
 	int pid  = atoi(argv[1]);
 	int ret = syscall(378, pid, fake_pgd, 1);
-	printf("fake_pgd : %lu", fake_pgd);
+	printf("fake_pgd : %lu", *fake_pgd);
 
-	int i;
-	for(i = 0; i < 2048 * 16; i += 16)
-	{
-		printf("fake_pgd: %lu \n", *(fake_pgd + i));
-	}
+	// int i;
+	// for(i = 0; i < 2048 * 16; i += 16)
+	// {
+	// 	printf("fake_pgd: %lu \n", *(fake_pgd + i));
+	// }
 //	printf("%d",ret);
 	return 0;
 }

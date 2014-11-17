2093b2a4cfc8d1e1d27d14660fea1f5cca77375c
xw2353 <xw2353@columbia.edu>
2014-11-16 20:44:14 -0500

    update flags

--- test.c
+++ test.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 	int fd;
 	unsigned long *fake_pgd;
 	fd = open("/dev/zero",O_RDWR); 
-	fake_pgd = (unsigned long *)mmap(NULL, 2048 * 8, PROT_READ, MAP_SHARED, fd, 0);
+	fake_pgd = (unsigned long *)mmap(NULL, 2048 * 8, PROT_READ, MAP_PRIVATE | MAP_ANONYMOUS, fd, 0);
 	int pid  = atoi(argv[1]);
 	int ret = syscall(378, pid, fake_pgd, 1);
 //	printf("%d",ret);
